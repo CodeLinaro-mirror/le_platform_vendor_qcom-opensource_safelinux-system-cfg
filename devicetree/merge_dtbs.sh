74de5b2c0c9acdaf33190cc05fd99056862ba136
Sankalp Negi <quic_snegi@quicinc.com>
2023-07-27 00:34:19 -0700

    safelinux-system-cfg: devicetree: Fix merge_dtbos.sh
    Fix issues with merge_dtbos.sh:
    1.) handle multiple modules as an array
    2.) Add DTS file presence check before reading them
    3.) Rename overlay file so as not to read it back when tabulating DTBs
    4.) Issues with common variable name
    5.) Remove commented/redundant code, and undesired prints(echo..).
    
    Change-Id: I2513f5e8d3d23c8b5e04cbf055f04dff9f27845b
    Signed-off-by: Sankalp Negi <quic_snegi@quicinc.com>

--- devicetree/merge_dtbs.sh
+++ devicetree/merge_dtbs.sh
@@ -18,44 +18,10 @@ KDIR=$1
 DTBO_DIR=$2
 OUT_DIR=$3
 
-dtb_files=$(find ${KDIR}/arch/arm64/boot/dts/qcom -name "*.dtb")
-
-dtbo_files=$(find $DTBO_DIR -name "*.dtbo")
-
-
-#dtbo_files=$(find ${KDIR}/arch/arm64/boot/dts/qcom -name "*.dtbo")
-
-create_dts()
-{
-        base_dts=$1
-        base_dtb=$2
-
-        ${KDIR}/scripts/dtc/dtc -I dtb -O dts -o $base_dts $base_dtb
-
-}
-
-#create dts from dtbs
-: <<'END_COMMENT'
-for base_dtb in $dtb_files
-do
-	base_dts=$(echo "$base_dtb" | sed -e 's/\.[^.]*$//')
-        base_dts="${base_dts}.dts"
-        #echo $file : create dts
-	create_dts $base_dts $base_dtb
-done
-END_COMMENT
-
-#build with support of overlay
-make -C ${KDIR} DTC_FLAGS="-@" -s  qcom/*.dtb
-
-#refresh the dtb list
-dtb_files=$(find ${KDIR}/arch/arm64/boot/dts/qcom -name "*.dtb")
-
 match_dtb_to_dtbo()
 {
 	dtb=$1
 	dtbo=$2
-	ret=1
 
 	dtbo_compatible=$(cat $dtbo | grep compatible | head -1 | sed -e 's/compatible =//' -e 's/\"//g' -e 's/[;\,]//g')
 	dtbo_model=$(cat $dtbo | grep model | head -1 | sed -e 's/model =//' -e 's/\"//g' -e 's/[;\,]//g')
@@ -66,27 +32,21 @@ match_dtb_to_dtbo()
 
 	if [ "$dtb_model" = "$dtbo_model" ]
 	then
-		ret=0
-		#echo model matched
-		return "$ret"
+		return 0
 	fi
 
 	for dtb in $dtb_compatible
 	do
 	for dtbo in $dtbo_compatible
 		do
-		#echo compare 1: $dtb with $dtbo
 			if [ "$dtb" = "$dtbo" ]
 			then
-				ret=0
-				#echo "1: match for $dtb with $dtbo"
-				return "$ret"
+				return 0
 			fi
 		done
 	done
-	return "$ret"
+	return 1
 }
-
 
 merge_dtbos()
 {
@@ -94,38 +54,51 @@ merge_dtbos()
 	base_dts_name=$(echo "$base_dtb" | sed -e 's/\.[^.]*$//')
 	base_dts="${base_dts_name}.dts"
 	dtbo_files=$2
-
 	matched_dtbos=""
 
+	# Proceed only if the expected DTS file is present
+	if [ ! -f $base_dts ]
+	then
+		return
+	fi
 	for dtbo_file in $dtbo_files
 	do
-	dtbs_file=$(echo "$dtbo_file" | sed -e 's/\.[^.]*$//')
-	dtbs_file="${dtbs_file}.dts"
-
-	if match_dtb_to_dtbo $base_dts $dtbs_file
-	then
-		matched_dtbos="${matched_dtbos} ${dtbo_file}"
-	fi
+		dtbs_file=$(echo "$dtbo_file" | sed -e 's/\.[^.]*$//')
+		dtbs_file="${dtbs_file}.dts"
+		if match_dtb_to_dtbo $base_dts $dtbs_file
+		then
+			matched_dtbos="${matched_dtbos} ${dtbo_file}"
+		fi
 	done
 
 	base_name=$(basename $base_dtb)
 	base_dts_name=$(echo "$base_name" | sed -e 's/\.[^.]*$//')
-	out_file="${base_dts_name}.overlay.dtb"
-
+	out_file="${base_dts_name}.dtb.overlay"
 
 	if [ "$matched_dtbos" != ""  ]
 	then
-		echo "=============================================================="
-		echo dtbos matches : $matched_dtbos
-		echo base dtb: $base_dtb
-		echo output file: ${OUT_DIR}/${out_file}
-		echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+		# execute the command in verbose mode(-v)
 		${KDIR}/scripts/dtc/fdtoverlay -i $base_dtb -o ${OUT_DIR}/${out_file} -v $matched_dtbos
+		#exit in case of failure
+		if [ $? -ne 0 ]; then
+			exit 1
+		fi
 	fi
 }
 
-for file in $dtb_files
-do
-	#echo $file
-	merge_dtbos $file $dtbo_files
-done
+main()
+{
+	#build with support of overlay
+	make -C ${KDIR} DTC_FLAGS="-@" -s qcom/*.dtb
+
+	#refresh the dtb list
+	dtb_files=$(find ${KDIR}/arch/arm64/boot/dts/qcom -name "*.dtb")
+	dtbo_files=$(find $DTBO_DIR -name "*.dtbo")
+
+	for file in $dtb_files
+	do
+		merge_dtbos $file "${dtbo_files[@]}"
+	done
+}
+
+main
