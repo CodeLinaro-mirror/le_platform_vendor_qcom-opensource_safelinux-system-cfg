1b91e7734f328ded77c3c2a81bd7d2afbac6774e
Venkatakrishnaiah Pari <quic_vpari@quicinc.com>
2023-08-13 22:02:38 -0700

    systemd-cfg: rename umd_glink device name in the to vfio-device-bind
    Rename umd_glink device name in the to vfio-device-bind script.
    
    Change-Id: Id563a9592b9bd9c101bb5196c6b0301dec34631c

--- vfio-device-probe/vfio-device-bind.sh
+++ vfio-device-probe/vfio-device-bind.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 
 DEVS="17c23000.vfio_timer 3d00000.vfio_kgsl ac00000.vfio_titan_base ab00000.vfio_eva aa00000.vfio_vidc ae00000.vfio_dpu_00 22000000.vfio_dpu_10
-	  786000.umd_glink 408000.umd_pil 90900000.subsystem_ramdump_util 1.cdsp0_cb 2.cdsp0_cb 3.cdsp0_cb 4.cdsp0_cb 1.cdsp1_cb 2.cdsp1_cb 3.cdsp1_cb 4.cdsp1_cb
+	  780000.umd_glink 408000.umd_pil 90900000.subsystem_ramdump_util 1.cdsp0_cb 2.cdsp0_cb 3.cdsp0_cb 4.cdsp0_cb 1.cdsp1_cb 2.cdsp1_cb 3.cdsp1_cb 4.cdsp1_cb
 	  2.adsp_cb 3.adsp_cb 4.adsp_cb 1.gpdsp0_cb 1.gpdsp1_cb 3.gpdsp0_cb 3.gpdsp1_cb soc:vfio_gmu soc:vfio_gmu_db soc:vfio_kgsl_secure soc:vfio_kgsl_lpac
 	  soc:vfio_vidc_non_secure_pixel_cb 1000.audio"
 
