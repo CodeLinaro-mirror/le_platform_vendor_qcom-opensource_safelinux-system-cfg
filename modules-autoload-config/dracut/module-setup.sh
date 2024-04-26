bd50e03b5df7c5ef6238150c2f2cab9b09b61d77
Nikunj Kela <quic_nkela@quicinc.com>
2024-04-25 19:26:01 -0700

    module-setup.sh: remove unwanted modules
    SCMI based SA8775p variant doesn't need these driver hence
    remove them.
    
    Change-Id: If523064d8d08755ad652104b87733fff3b597823
    Signed-off-by: Nikunj Kela <quic_nkela@quicinc.com>

--- modules-autoload-config/dracut/module-setup.sh
+++ modules-autoload-config/dracut/module-setup.sh
@@ -17,16 +17,6 @@ installkernel() {
 # list of kernel modules to load during initramfs boot
 # Necessary drivers for bringing up console and UFS based RootFS$
 SA8775_INITRAMFS_DRIVERS=" \
-    camcc-sa8775p \
-    dispcc0-sa8775p \
-    dispcc1-sa8775p \
-    gpucc-sa8775p \
-    videocc-sa8775p \
-    spmi-pmic-arb \
-    pinctrl-spmi-gpio \
-    pinctrl-spmi-mpp \
-    nvmem_qcom-spmi-sdam \
-    qcom-spmi-temp-alarm \
     virtio_mmio \
     virtio_net \
     failover \
