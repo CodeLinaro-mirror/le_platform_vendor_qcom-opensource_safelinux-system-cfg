f472fbb024579c332408cdbe0736f049c0365dc7
ziqic <quic_ziqic_01@quicinc.com>
2024-12-25 23:01:49 -0800

    Revert "vfio-device-probe:skip operations"
    This reverts commit 41b96c20172c2d98bcb41cee53af7164d73448d6.
    
    Reason for revert: 8797 add another mm-vfio-device-bind.sh which runs before this script, and it does almost the same things, which makes the judgement wrong. Change ExecStart directly to optimize vfio service.
    
    Change-Id: I8cc4e314be4dfb60e1799902a771bde3acd10331
    Signed-off-by: ziqic <quic_ziqic_01@quicinc.com>

--- vfio-device-probe/vfio-device-bind.sh
+++ vfio-device-probe/vfio-device-bind.sh
@@ -4,17 +4,10 @@
 
 # shellcheck disable=SC1090
 
-KGSL_OVERRIDE_DIR="/sys/bus/platform/devices/1.vfio_kgsl_lpac/driver_override"
-
-if [ "$(grep -c "vfio-platform" "$KGSL_OVERRIDE_DIR")" -ne '0' ];then
-    echo "vfio-device-bind.sh already run in early-ramdisk-init"
-    exit 0
-fi
-
 echo "1.vfio_kgsl" > /sys/bus/platform/drivers/kiumd_kgsl/unbind
 echo "1.vfio_kgsl_lpac" > /sys/bus/platform/drivers/kiumd_kgsl/unbind
 
-echo "vfio-platform" > $KGSL_OVERRIDE_DIR
+echo "vfio-platform" > /sys/bus/platform/devices/1.vfio_kgsl_lpac/driver_override
 echo "1.vfio_kgsl_lpac" > /sys/bus/platform/drivers/vfio-platform/bind
 
 # Assume only one conf file is present in /usr/lib/vfio-bind.d
